072944ffd929eec22a40ef669176838662cf029d
aksarlaode <92644469+aksarlaode@users.noreply.github.com>
2023-05-15 06:17:28 +0000

    hg

--- apps/nextjs/src/app/(auth)/signin/page.tsx
+++ apps/nextjs/src/app/(auth)/signin/page.tsx
@@ -103,7 +103,7 @@ export default function AuthenticationPage() {
             </blockquote>
           </div>
         </div>
-        <div className="flex h-full items-center lg:p-8">
+        <div className="flex h-full lg:p-8">
           <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
             <div className="flex flex-col space-y-2 text-center">
               <Icons.logo className="mx-auto h-6 w-6" />
