242836e5df5b1ba7478bc0d0a76eeb4738fc29f0
aksarlaode <92644469+aksarlaode@users.noreply.github.com>
2023-05-15 00:13:57 +0000

    auth

--- apps/nextjs/src/app/(auth)/signin/page.tsx
+++ apps/nextjs/src/app/(auth)/signin/page.tsx
@@ -62,19 +62,6 @@ export default function AuthenticationPage() {
     </div>
   */}
       <div className="container relative hidden h-[800px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
-        <Link
-          href="/"
-          className={cn(
-            buttonVariants({ variant: "ghost" }),
-            "absolute left-4 top-4 md:left-8 md:top-8",
-          )}
-        >
-          <>
-            <Icons.chevronLeft className="mr-2 h-4 w-4" />
-            Back
-          </>
-        </Link>
-
         <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
           <div
             className="absolute inset-0 bg-cover"
@@ -98,7 +85,19 @@ export default function AuthenticationPage() {
           </div>
         </div>
       </div>
-      <div className="lg:p-8">
+      <div className="container flex h-screen w-screen flex-col items-center justify-center">
+        <Link
+          href="/"
+          className={cn(
+            buttonVariants({ variant: "ghost" }),
+            "absolute left-4 top-4 md:left-8 md:top-8",
+          )}
+        >
+          <>
+            <Icons.chevronLeft className="mr-2 h-4 w-4" />
+            Back
+          </>
+        </Link>
         <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
           <div className="flex flex-col space-y-2 text-center">
             <Icons.logo className="mx-auto h-6 w-6" />
--- apps/nextjs/src/app/(auth)/signout/page.tsx
+++ apps/nextjs/src/app/(auth)/signout/page.tsx
@@ -13,7 +13,7 @@ export default function AuthenticationPage() {
   const router = useRouter();
 
   return (
-    <div className="container h-[800px] flex-col items-center justify-center">
+    <div className="container flex h-screen w-screen flex-col items-center justify-center">
       <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
         <div className="flex flex-col space-y-2 text-center">
           <h1 className="text-2xl font-semibold tracking-tight">Sign Out</h1>
