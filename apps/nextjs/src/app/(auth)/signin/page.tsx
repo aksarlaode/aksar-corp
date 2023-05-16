abdc020c53e854a3d11078fa11af480d2302c926
aksarlaode <92644469+aksarlaode@users.noreply.github.com>
2023-05-16 23:33:03 +0000

    edit

--- apps/nextjs/src/app/(auth)/signin/page.tsx
+++ apps/nextjs/src/app/(auth)/signin/page.tsx
@@ -19,137 +19,90 @@ export const metadata: Metadata = {
 
 export default function AuthenticationPage() {
   return (
-    <>
-      {/* <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
-        <div className="flex flex-col space-y-2 text-center">
-          <h1 className="text-2xl font-semibold tracking-tight">
-            Create an account
-          </h1>
-          <p className="text-sm text-muted-foreground">
-            Enter your email below to create your account
-          </p>
-        </div>
-        <div className="grid gap-6">
-          <EmailSignIn />
-
-          <div className="relative">
-            <div className="absolute inset-0 flex items-center">
-              <span className="w-full border-t" />
-            </div>
-            <div className="relative flex justify-center text-xs uppercase">
-              <span className="bg-background px-2 text-muted-foreground">
-                Or continue with
-              </span>
-            </div>
-          </div>
-
-          <OAuthSignIn />
-        </div>
-
-        <p className="px-8 text-center text-sm text-muted-foreground">
-          By clicking continue, you agree to our{" "}
-          <Link
-            href={"/terms" as Route}
-            className="underline underline-offset-4 hover:text-primary"
-          >
-            Terms of Service
-          </Link>{" "}
-          and{" "}
-          <Link
-            href={"/privacy" as Route}
-            className="underline underline-offset-4 hover:text-primary"
-          >
-            Privacy Policy
-          </Link>
-          .
-        </p>
-      </div>*/}
-
-      <div className="container relative h-[800px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+    <div className="container relative h-[800px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+      <Link
+        href="/"
+        className={cn(
+          buttonVariants({ variant: "ghost" }),
+          "absolute left-4 top-4 md:hidden",
+        )}
+      >
+        <>
+          <Icons.chevronLeft className="mr-2 h-4 w-4" />
+          Back
+        </>
+      </Link>
+      <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
+        <div
+          className="absolute inset-0 bg-cover"
+          style={{
+            backgroundImage:
+              "url(https://images.unsplash.com/photo-1590069261209-f8e9b8642343?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1376&q=80)",
+          }}
+        />
         <Link
           href="/"
-          className={cn(
-            buttonVariants({ variant: "ghost" }),
-            "absolute left-4 top-4 md:hidden",
-          )}
+          className="relative z-20 flex items-center text-lg font-medium"
         >
-          <>
-            <Icons.chevronLeft className="mr-2 h-4 w-4" />
-            Back
-          </>
+          <Command className="mr-2 h-6 w-6" /> Acme Inc
         </Link>
-        <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
-          <div
-            className="absolute inset-0 bg-cover"
-            style={{
-              backgroundImage:
-                "url(https://images.unsplash.com/photo-1590069261209-f8e9b8642343?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1376&q=80)",
-            }}
-          />
-          <Link
-            href="/"
-            className="relative z-20 flex items-center text-lg font-medium"
-          >
-            <Command className="mr-2 h-6 w-6" /> Acme Inc
-          </Link>
-          <div className="relative z-20 mt-auto">
-            <blockquote className="space-y-2">
-              <p className="text-lg">
-                &ldquo;This library has saved me countless hours of work and
-                helped me deliver stunning designs to my clients faster than
-                ever before. Highly recommended!&rdquo;
-              </p>
-              <footer className="text-sm">Sofia Davis</footer>
-            </blockquote>
-          </div>
-        </div>
-        <div className="flex h-full lg:p-8">
-          <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
-            <div className="flex flex-col space-y-2 text-center">
-              <Icons.logo className="mx-auto h-6 w-6" />
-              <h1 className="text-2xl font-semibold tracking-tight">
-                Welcome back
-              </h1>
-              <p className="text-sm text-muted-foreground">
-                Enter your email to sign in to your account
-              </p>
-            </div>
-            <div className="grid gap-6">
-              <EmailSignIn />
-
-              <div className="relative">
-                <div className="absolute inset-0 flex items-center">
-                  <span className="w-full border-t" />
-                </div>
-                <div className="relative flex justify-center text-xs uppercase">
-                  <span className="bg-background px-2 text-muted-foreground">
-                    Or continue with
-                  </span>
-                </div>
-              </div>
-
-              <OAuthSignIn />
-            </div>
-            <p className="px-8 text-center text-sm text-muted-foreground">
-              By clicking continue, you agree to our{" "}
-              <Link
-                href={"/terms" as Route}
-                className="underline underline-offset-4 hover:text-primary"
-              >
-                Terms of Service
-              </Link>{" "}
-              and{" "}
-              <Link
-                href={"/privacy" as Route}
-                className="underline underline-offset-4 hover:text-primary"
-              >
-                Privacy Policy
-              </Link>
-              .
+        <div className="relative z-20 mt-auto">
+          <blockquote className="space-y-2">
+            <p className="text-lg">
+              &ldquo;This library has saved me countless hours of work and
+              helped me deliver stunning designs to my clients faster than ever
+              before. Highly recommended!&rdquo;
             </p>
-          </div>
+            <footer className="text-sm">Sofia Davis</footer>
+          </blockquote>
         </div>
       </div>
-    </>
+      <div className="flex h-full lg:p-8">
+        <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+          <div className="flex flex-col space-y-2 text-center">
+            <Icons.logo className="mx-auto h-6 w-6" />
+            <h1 className="text-2xl font-semibold tracking-tight">
+              Welcome back
+            </h1>
+            <p className="text-sm text-muted-foreground">
+              Enter your email to sign in to your account
+            </p>
+          </div>
+          <div className="grid gap-6">
+            <EmailSignIn />
+
+            <div className="relative">
+              <div className="absolute inset-0 flex items-center">
+                <span className="w-full border-t" />
+              </div>
+              <div className="relative flex justify-center text-xs uppercase">
+                <span className="bg-background px-2 text-muted-foreground">
+                  Or continue with
+                </span>
+              </div>
+            </div>
+
+            <OAuthSignIn />
+          </div>
+          <p className="px-8 text-center text-sm text-muted-foreground">
+            By clicking continue, you agree to our{" "}
+            <Link
+              href={"/terms" as Route}
+              className="underline underline-offset-4 hover:text-primary"
+            >
+              Terms of Service
+            </Link>{" "}
+            and{" "}
+            <Link
+              href={"/privacy" as Route}
+              className="underline underline-offset-4 hover:text-primary"
+            >
+              Privacy Policy
+            </Link>
+            .
+          </p>
+        </div>
+      </div>
+    </div>
   );
 }
--- apps/nextjs/src/app/(editor)/editor/layout.tsx
+++ apps/nextjs/src/app/(editor)/editor/layout.tsx
@@ -1,20 +1,35 @@
-import { Suspense } from "react";
+import React, { Suspense } from "react";
+import Link from "next/link";
+
+import { cn } from "@aksar/ui";
+import { buttonVariants } from "@aksar/ui/button";
+import { Icons } from "@aksar/ui/icons";
 
 import { SiteFooter } from "~/components/footer";
-import { MainNav } from "~/components/main-nav";
+//import { MainNav } from "~/components/main-nav";
 import { UserNav } from "~/components/user-nav";
-import { navItems } from "../../config";
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+//import { navItems } from "../../config";
+
+export default function RootLayout({ children }: React.PropsWithChildren) {
   return (
     <div className="flex min-h-screen flex-col">
       <header className="container z-40 bg-background">
         <div className="flex h-20 items-center justify-between py-6">
-          <MainNav items={navItems} />
+          {/* <MainNav items={navItems} /> */}
+          <Link
+            href="/"
+            className={cn(
+              buttonVariants({ variant: "ghost" }),
+              /*"absolute left-4 top-4 md:hidden",*/
+            )}
+          >
+            <>
+              <Icons.chevronLeft className="mr-2 h-4 w-4" />
+              Back
+            </>
+          </Link>
+
           <nav>
             <Suspense>
               {/* @ts-expect-error - ... */}
