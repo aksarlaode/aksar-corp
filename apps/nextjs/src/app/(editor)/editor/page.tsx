ac9b890af4bf1a60714d6a3928df789446357382
aksarlaode <92644469+aksarlaode@users.noreply.github.com>
2023-05-16 12:15:32 +0000

    client

--- apps/nextjs/src/app/(editor)/editor/page.tsx
+++ apps/nextjs/src/app/(editor)/editor/page.tsx
@@ -1,9 +1,9 @@
+"use client";
+
 import RichTextEditor from "~/components/editor";
 
 export function EditorLayout() {
   function onChange() {
-    "use server";
-
     console.log("test");
   }
   return <RichTextEditor initialValue="<h1>Hello World" onChange={onChange} />;
