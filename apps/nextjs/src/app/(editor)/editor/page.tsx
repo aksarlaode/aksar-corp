3ec8d5c11f7c41f298c9dc8147340b680d45091b
aksarlaode <92644469+aksarlaode@users.noreply.github.com>
2023-05-17 00:19:18 +0000

    menubar

--- apps/nextjs/src/app/(editor)/editor/page.tsx
+++ apps/nextjs/src/app/(editor)/editor/page.tsx
@@ -9,10 +9,15 @@ import {
   type Editor,
 } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
-import { Bold, Heading1, Italic, Strikethrough } from "lucide-react";
+import {
+  Bold,
+  Heading1,
+  Heading2,
+  Heading3,
+  Italic,
+  Strikethrough,
+} from "lucide-react";
 
-import { Button } from "@aksar/ui/button";
-import { Card, CardContent } from "@aksar/ui/card";
 import { Menubar, MenubarMenu, MenubarTrigger } from "@aksar/ui/menubar";
 
 const BubbleMenuBar = ({ editor }: { editor: Editor }) => {
@@ -58,28 +63,36 @@ const FloatingMenuBar = ({ editor }: { editor: Editor }) => {
     <FloatingMenu editor={editor} tippyOptions={{ duration: 100 }}>
       <Menubar className="flex flex-col">
         <MenubarMenu>
-          <MenubarTrigger>Menu</MenubarTrigger>
-        </MenubarMenu>
-        <MenubarMenu>
-          <MenubarTrigger>Menu</MenubarTrigger>
-        </MenubarMenu>
-        <MenubarMenu>
-          <MenubarTrigger>Menu</MenubarTrigger>
-        </MenubarMenu>
-      </Menubar>
-      <Card>
-        <CardContent>
-          <Button
-            variant="ghost"
+          <MenubarTrigger
             onClick={() =>
               editor.chain().focus().toggleHeading({ level: 1 }).run()
             }
           >
             <Heading1 className="mr-2 h-4 w-4" />
             <span>Heading 1</span>
-          </Button>
-        </CardContent>
-      </Card>
+          </MenubarTrigger>
+        </MenubarMenu>
+        <MenubarMenu>
+          <MenubarTrigger
+            onClick={() =>
+              editor.chain().focus().toggleHeading({ level: 2 }).run()
+            }
+          >
+            <Heading2 className="mr-2 h-4 w-4" />
+            <span>Heading 2</span>
+          </MenubarTrigger>
+        </MenubarMenu>
+        <MenubarMenu>
+          <MenubarTrigger
+            onClick={() =>
+              editor.chain().focus().toggleHeading({ level: 3 }).run()
+            }
+          >
+            <Heading3 className="mr-2 h-4 w-4" />
+            <span>Heading 3</span>
+          </MenubarTrigger>
+        </MenubarMenu>
+      </Menubar>
     </FloatingMenu>
   );
 };
