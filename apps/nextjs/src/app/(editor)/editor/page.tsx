6e7adf61bf4c8bd11f702fdcc9c5eee3b12f5fdc
aksarlaode <92644469+aksarlaode@users.noreply.github.com>
2023-05-17 03:37:46 +0000

    new

--- apps/nextjs/src/app/(editor)/editor/page.tsx
+++ apps/nextjs/src/app/(editor)/editor/page.tsx
@@ -12,10 +12,18 @@ import StarterKit from "@tiptap/starter-kit";
 import {
   Bold,
   Code,
+  Code2,
   Heading1,
   Heading2,
   Heading3,
+  Heading4,
+  Heading5,
+  Heading6,
   Italic,
+  List,
+  ListOrdered,
+  Minus,
+  Quote,
   Strikethrough,
 } from "lucide-react";
 
@@ -117,6 +125,81 @@ const FloatingMenuBar = ({ editor }: { editor: Editor }) => {
             <Heading3 className="mr-2 h-4 w-4" />
             <span>Heading 3</span>
           </Button>
+          <Button
+            variant="ghost"
+            onClick={() =>
+              editor.chain().focus().toggleHeading({ level: 4 }).run()
+            }
+            className={
+              editor.isActive("heading", { level: 4 }) ? "bg-secondary" : ""
+            }
+          >
+            <Heading4 className="mr-2 h-4 w-4" />
+            <span>Heading 4</span>
+          </Button>
+          <Button
+            variant="ghost"
+            onClick={() =>
+              editor.chain().focus().toggleHeading({ level: 5 }).run()
+            }
+            className={
+              editor.isActive("heading", { level: 5 }) ? "bg-secondary" : ""
+            }
+          >
+            <Heading5 className="mr-2 h-4 w-4" />
+            <span>Heading 5</span>
+          </Button>
+          <Button
+            variant="ghost"
+            onClick={() =>
+              editor.chain().focus().toggleHeading({ level: 6 }).run()
+            }
+            className={
+              editor.isActive("heading", { level: 6 }) ? "bg-secondary" : ""
+            }
+          >
+            <Heading6 className="mr-2 h-4 w-4" />
+            <span>Heading 6</span>
+          </Button>
+          <Button
+            variant="ghost"
+            onClick={() => editor.chain().focus().toggleBulletList().run()}
+            className={editor.isActive("bulletList") ? "bg-secondary" : ""}
+          >
+            <List className="mr-2 h-4 w-4" />
+            <span>Bullet List</span>
+          </Button>
+          <Button
+            variant="ghost"
+            onClick={() => editor.chain().focus().toggleOrderedList().run()}
+            className={editor.isActive("orderedList") ? "bg-secondary" : ""}
+          >
+            <ListOrdered className="mr-2 h-4 w-4" />
+            <span>Ordered List</span>
+          </Button>
+          <Button
+            variant="ghost"
+            onClick={() => editor.chain().focus().toggleCodeBlock().run()}
+            className={editor.isActive("codeBlock") ? "bg-secondary" : ""}
+          >
+            <Code2 className="mr-2 h-4 w-4" />
+            <span>Code Block</span>
+          </Button>
+          <Button
+            variant="ghost"
+            onClick={() => editor.chain().focus().toggleBlockquote().run()}
+            className={editor.isActive("blockquote") ? "bg-secondary" : ""}
+          >
+            <Quote className="mr-2 h-4 w-4" />
+            <span> Blockquote</span>
+          </Button>
+          <Button
+            variant="ghost"
+            onClick={() => editor.chain().focus().setHorizontalRule().run()}
+          >
+            <Minus className="mr-2 h-4 w-4" />
+            <span> horizontal rule</span>
+          </Button>
         </CardContent>
       </Card>
     </FloatingMenu>
@@ -125,7 +208,18 @@ const FloatingMenuBar = ({ editor }: { editor: Editor }) => {
 
 const EditorPage = () => {
   const editor = useEditor({
-    extensions: [StarterKit],
+    extensions: [
+      StarterKit.configure({
+        bulletList: {
+          keepMarks: true,
+          keepAttributes: false, // TODO : Making this as `false` becase marks are not preserved when I try to preserve attrs, awaiting a bit of help
+        },
+        orderedList: {
+          keepMarks: true,
+          keepAttributes: false, // TODO : Making this as `false` becase marks are not preserved when I try to preserve attrs, awaiting a bit of help
+        },
+      }),
+    ],
     content: `<p>Hello World</p>`,
     editorProps: {
       attributes: {
