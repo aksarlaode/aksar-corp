72ab3dc64a106cc993677c8b00757acfd925be1d
aksarlaode <92644469+aksarlaode@users.noreply.github.com>
2023-05-18 04:23:41 +0000

    Fix/nativewind ts config

---
+++ apps/expo/tailwind.config.ts
@@ -0,0 +1,8 @@
+import { type Config } from "tailwindcss";
+
+import baseConfig from "@aksar/tailwind-config";
+
+export default {
+  presets: [baseConfig],
+  content: ["./app/**/*.{ts,tsx}", "./src/**/*.{ts,tsx}"],
+} satisfies Config;
