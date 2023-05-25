acc9ab7ac1581aa0748c6f9657c7122d418a0b5e
aksarlaode <akhsaras21@gmail.com>
2023-05-25 09:26:39 +0000

    fix error

--- apps/nextjs/src/app/(auth)/signin/page.tsx
+++ apps/nextjs/src/app/(auth)/signin/page.tsx
@@ -1,7 +1,8 @@
 import type { Metadata, Route } from "next";
 import Link from "next/link";
 
-import { Button } from "@aksar/ui/button";
+import { cn } from "@aksar/ui";
+import { buttonVariants } from "@aksar/ui/button";
 import { Command } from "@aksar/ui/command";
 import { Icons } from "@aksar/ui/icons";
 
@@ -19,18 +20,18 @@ export const metadata: Metadata = {
 export default function AuthenticationPage() {
   return (
     <div className="container relative h-[800px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
-      <Button
-        variant="ghost"
-        asChild
-        className="absolute left-4 top-4 md:hidden"
+      <Link
+        href="/"
+        className={cn(
+          buttonVariants({ variant: "ghost" }),
+          "absolute left-4 top-4 md:hidden",
+        )}
       >
-        <Link href="/">
-          <>
-            <Icons.chevronLeft className="mr-2 h-4 w-4" />
-            Back
-          </>
-        </Link>
-      </Button>
+        <>
+          <Icons.chevronLeft className="mr-2 h-4 w-4" />
+          Back
+        </>
+      </Link>
       <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
         <div
           className="absolute inset-0 bg-cover"
--- apps/nextjs/src/app/(editor)/editor/page.tsx
+++ apps/nextjs/src/app/(editor)/editor/page.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import Link from "next/link";
 
-import { Button } from "@aksar/ui/button";
+import { cn } from "@aksar/ui";
+import { buttonVariants } from "@aksar/ui/button";
 import { Icons } from "@aksar/ui/icons";
 
 import Content from "./content";
@@ -11,14 +12,18 @@ export const revalidate = 60;
 const Post = () => {
   return (
     <div className="container relative h-screen w-screen flex-col">
-      <Button variant="ghost" asChild className="absolute left-4 top-4">
-        <Link href="/">
-          <>
-            <Icons.chevronLeft className="mr-2 h-4 w-4" />
-            Back
-          </>
-        </Link>
-      </Button>
+      <Link
+        href="/"
+        className={cn(
+          buttonVariants({ variant: "ghost" }),
+          "absolute left-4 top-4",
+        )}
+      >
+        <>
+          <Icons.chevronLeft className="mr-2 h-4 w-4" />
+          Back
+        </>
+      </Link>
 
       <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
         <Content />
