bb140b1273edf68e5f9628c524ce15d23957e1f1
aksarlaode <akhsaras21@gmail.com>
2023-05-27 00:22:34 +0000

    refactor

--- apps/nextjs/src/app/(editor)/editor/content.tsx
+++ apps/nextjs/src/app/(editor)/editor/content.tsx
@@ -1,16 +1,34 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 "use client";
 
 import React, { useState } from "react";
+
+
 
 import Highlight from "@tiptap/extension-highlight";
 import Placeholder from "@tiptap/extension-placeholder";
 import { EditorContent, useEditor, type Editor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 
+
+
+import { api } from "~/utils/api";
+
+
+
 import { BubbleMenuBar, FloatingMenuBar, MenuBar } from "./menu";
+
+
+export type ChatItem = {
+  author: "User" | "AI";
+  content: string;
+  isError?: boolean;
+};
 
 const Content = () => {
   const [content, setContent] = useState<string>("");
+  const [chatItems, setChatItems] = useState<ChatItem[]>([]);
+const[waiting,setWaiting]=useState(false)
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const handleOnChangeContent = ({ editor }: any) => {
@@ -47,9 +65,57 @@ const Content = () => {
     onUpdate: handleOnChangeContent,
   });
 
+    if (!editor) {
+    return null;
+  }
+
+  const generatedTextMutation = api.ai.generateText.useMutation({
+    onSuccess: (data) => {
+      setChatItems([
+        ...chatItems,
+        {
+          content: data.generatedText,
+          author: "AI",
+        },
+      ]);
+      editor.chain().focus().setContent(chatItems[0]!.content).run();
+      setContent(chatItems[0]!.content);
+    },
+
+    onError: (error) => {
+      setChatItems([
+        ...chatItems,
+        {
+          content: error.message ?? "An error occurred",
+          author: "AI",
+          isError: true,
+        },
+      ]);
+    },
+
+    onSettled: () => {
+      setWaiting(false);
+    },
+  });
+
+    const handleUpdate = (prompt: string) => {
+      setWaiting(true);
+
+      setChatItems([
+        ...chatItems,
+        {
+          content: prompt.replace(/\n/g, "\n\n"),
+          author: "User",
+        },
+      ]);
+
+      generatedTextMutation.mutate({ prompt });
+    };
+
+
   return (
     <div>
-      {editor && <MenuBar setContent={setContent} editor={editor} />}
+      {editor && <MenuBar waiting={waiting} onUpdate={handleUpdate} editor={editor} />}
       {editor && <BubbleMenuBar editor={editor} />}
       {editor && <FloatingMenuBar editor={editor} />}
       <EditorContent editor={editor} />
--- apps/nextjs/src/app/(editor)/editor/menu.tsx
+++ apps/nextjs/src/app/(editor)/editor/menu.tsx
@@ -1,12 +1,22 @@
 import { useState } from "react";
 
 import { BubbleMenu, FloatingMenu, type Editor } from "@tiptap/react";
-import { MoreHorizontalIcon, Redo2, Undo2 } from "lucide-react";
+import { MoreHorizontalIcon, Redo2, SendIcon, Undo2 } from "lucide-react";
 
 import { Button } from "@aksar/ui/button";
 import { Card, CardContent } from "@aksar/ui/card";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@aksar/ui/dialog";
+import { Input } from "@aksar/ui/input";
 
-import AskAI from "./ask-ai";
 import { bubbleMenus, floatingMenus } from "./menubar";
 
 export const BubbleMenuBar = ({ editor }: { editor: Editor }) => {
@@ -64,16 +74,23 @@ export const FloatingMenuBar = ({ editor }: { editor: Editor }) => {
 
 export const MenuBar = ({
   editor,
-  setContent,
+  waiting,
+  onUpdate,
 }: {
   editor: Editor | null;
-  setContent: (content: string) => void;
+  waiting: boolean;
+  onUpdate: (prompt: string) => void;
 }) => {
-  const [AIThinking, setAIThinking] = useState(false);
+  const [prompt, setPrompt] = useState<string>("");
 
   if (!editor) {
     return null;
   }
+
+  const handleUpdate = () => {
+    setPrompt("");
+    onUpdate(prompt);
+  };
 
   return (
     <div className="flex">
@@ -93,12 +110,45 @@ export const MenuBar = ({
       >
         <Redo2 className="h-4 w-4" />
       </Button>
-      <AskAI
-        setContent={setContent}
-        editor={editor}
-        setAIThinking={setAIThinking}
-      />
-      {AIThinking && (
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button variant="outline" size="sm">
+            Ask AI!
+          </Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>Generate AI Content</DialogTitle>
+            <DialogDescription>
+              What type of writer do you want?{" "}
+            </DialogDescription>
+          </DialogHeader>
+          <Input
+            autoComplete="off"
+            spellCheck={false}
+            className="h-12 w-full"
+            placeholder="Ask something..."
+            autoFocus
+            name="user_prompt"
+            value={prompt}
+            onChange={(e) => setPrompt(e.target.value)}
+            onKeyDown={(e) => {
+              if (e.key === "Enter" && !e.shiftKey) {
+                handleUpdate();
+              }
+            }}
+          />
+
+          <DialogFooter>
+            <DialogClose asChild>
+              <Button type="submit" onClick={handleUpdate}>
+                <SendIcon className="h-5 w-5 text-gray-400" />
+              </Button>
+            </DialogClose>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+      {waiting && (
         <Button variant="ghost" size="sm" className="ml-auto">
           <MoreHorizontalIcon className="h-4 w-4 animate-pulse" />
         </Button>
