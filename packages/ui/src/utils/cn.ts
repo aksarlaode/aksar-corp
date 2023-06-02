65c3a1751c341c0ac21a3d6de3f1463dcc6ab594
aksarlaode <akhsaras21@gmail.com>
2023-06-02 15:48:52 +0000

    feat: remove clsx library and utilize cx feature from cva library

--- packages/ui/src/utils/cn.ts
+++ packages/ui/src/utils/cn.ts
@@ -1,8 +1,8 @@
-import type { ClassValue } from "clsx";
+import type { CxOptions } from "class-variance-authority";
 
-import { clsx } from "clsx";
+import { cx } from "class-variance-authority";
 import { twMerge } from "tailwind-merge";
 
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+export function cn(...inputs: CxOptions) {
+  return twMerge(cx(inputs));
 }
